ad1ba626101b93dcf1f79631a1105a23936bb67f
Tatan <jorge.tatan@gmail.com>
2018-12-08 22:01:45 -0300

    Modelo inicial de Tienda

--- apps/tienda/models.py
+++ apps/tienda/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Tienda(models.Model):
+    nombre = models.CharField(max_length=50)
+    pendiente = models.BooleanField(default=True)
+
+
