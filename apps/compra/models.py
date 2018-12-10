a4d3259adbc0553a85164f36746868dbbd163fae
Tatan <jorge.tatan@gmail.com>
2018-12-10 06:30:16 -0300

    Correcciones a models

--- apps/compra/models.py
+++ apps/compra/models.py
@@ -6,23 +6,27 @@ from apps.tienda.models import Tienda
 # Create your models here.
 
 class ProLista(models.Model):
-    nombre = models.CharField(max_length=30)
     producto = models.ForeignKey(Producto, null=True, blank=True, on_delete=models.CASCADE)
-    costo_presupuestado = models.IntegerField()
-    cantidad = models.IntegerField()
-    costo_real = models.IntegerField()
+    cantidad = models.PositiveIntegerField(default=1)
+    costo_presupuestado = models.IntegerField(null=True, blank=True)
+    total_presupuestado = models.IntegerField(null=True, blank=True)
     tienda = models.ForeignKey(Tienda, null=True, blank=True, on_delete=models.CASCADE)
+    costo_real = models.IntegerField(null=True, blank=True)
+    total_real = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.producto)
 
 
 class Lista(models.Model):
     nombre = models.CharField(max_length=50)
-    lista_productos = models.ForeignKey('ProLista', null=True, blank=True, on_delete=models.CASCADE)
+    lista_productos = models.ForeignKey(ProLista, null=True, blank=True, on_delete=models.CASCADE)
     totalagregados = models.IntegerField()
     totalpresupuestado = models.IntegerField()
     finalizada = models.BooleanField(default=False)
 
 
 class Compra(models.Model):
-    lista = models.ForeignKey('Lista', null=True, blank=True)
+    lista = models.ForeignKey('Lista', null=True, blank=True, on_delete=models.CASCADE)
     totalreal = models.IntegerField()
     totalfinal = models.IntegerField()
