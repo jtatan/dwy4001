a4106cba38d4f681d39d6478967f2804e85605ce
GiulianoRB <giuliano.developer@gmail.com>
2018-12-10 23:48:03 -0300

    login normal completo

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
 
-app_nme = 'accounts'
+app_name = 'accounts'
 
 urlpatterns = [
     url(r'^signup/$', views.signup_view, name="signup"),
+    url(r'^login/$', views.login_view, name="login"),
+    url(r'^logout/$', views.logout_view, name ="logout"),
 ]
